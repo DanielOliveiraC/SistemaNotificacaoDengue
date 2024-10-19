42f03864964a3a373e6b49d8b8e00bbbd2d60094
DanielOliveiraC <danieloliveira012005@gmail.com>
2024-10-19 11:25:44 -0300

    comit

---
+++ src/main/java/com/ifpi/fichadengue/FichaDengueApplication.java
@@ -0,0 +1,13 @@
+package com.ifpi.fichadengue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaDengueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FichaDengueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifpi/fichadengue/FichaDengueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpi.fichadengue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaDengueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
