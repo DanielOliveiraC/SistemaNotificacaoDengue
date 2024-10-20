91037e9e2544187e78eec20d1f2e0288c5dc59f5
DanielOliveiraC <danieloliveira012005@gmail.com>
2024-10-19 23:54:24 -0300

    Update fichaController.java
    add sistema de salvar

--- fichaDengue/src/main/java/com/ifpi/fichadengue/controllers/fichaController.java
+++ fichaDengue/src/main/java/com/ifpi/fichadengue/controllers/fichaController.java
@@ -1,13 +1,34 @@
 package com.ifpi.fichadengue.controllers;
 
+import com.ifpi.fichadengue.models.GeneralDataModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class fichaController {
 
+    private List<GeneralDataModel> generalDataModelList = new ArrayList<>();
+
     @GetMapping("/notificar")
-    public String notificar() {
+    public String exibirNotificacao(Model model) {
+        model.addAttribute("generalDataModel", new GeneralDataModel());
         return "notify";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute GeneralDataModel generalDataModel, Model model) {
+        generalDataModel.setId(UUID.randomUUID());
+        generalDataModelList.add(generalDataModel);
+        String ver = generalDataModel.toString();
+        System.out.println(ver);
+        return "redirect:/notificar";
     }
 }
